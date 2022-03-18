8b56a4340c0cc92c3fd5652dd2987bcfa7a62839
Liz Koberle <lizkoberle@gmail.com>
2022-03-18 15:58:18 -0400

    getting things set up

---
+++ client/App.vue
@@ -0,0 +1,27 @@
+<template>
+ <div id="app">
+   <h1>{{ msg }}</h1>
+   <div class="row" id="eventList">
+       <h2>Event List</h2>
+ </div>
+ </div>
+</template>
+
+
+<script>
+export default {
+ name: "app",
+ computed: {
+ }
+};
+</script>
+
+<style lang="css">
+#app h2 {
+ font-size: 2rem;
+}
+.datetime-label {
+ color: #9e9e9e;
+ font-size: .8rem;
+}
+</style>
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -2,8 +2,10 @@
 
 const config = require('dotenv').config();
 const postgres = require('hapi-postgres-connection');
+const Vision = require('vision');
+const inert = require('inert');
 
 module.exports.register = async server => {
-	await server.register(postgres);
+	await server.register([ postgres, Vision, inert ]);
 };
 
